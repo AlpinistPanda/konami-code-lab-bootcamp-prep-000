ab89c1a2bc0f4583906b2e1527a26fdf313fef26
Ozgun Balaban <ozgunbalaban@yahoo.com>
2019-10-19 11:57:50 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,15 +1,4 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Keys for A, B, and C keys.
