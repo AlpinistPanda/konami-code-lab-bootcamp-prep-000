13cda9f21023b24beb3b141300747664ae412c0a
Ozgun Balaban <ozgunbalaban@yahoo.com>
2019-10-19 11:58:39 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,26 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Keys for A, B, and C keys.
-const alphabet = ['a', 'b', 'c'];
- 
-// Keep track of index outside of the event handler.
-let index = 0;
- 
-// This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
-  const key = e.key;
- 
-  if (key === alphabet[index]) {
-    index++;
- 
-    if (index === alphabet.length) {
-      alert("Hurray!");
- 
-      index = 0;
+var index = 0
+  document.body.addEventListener('keydown', onKeyDownHandler);
+
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+    console.log('key', key)
+    if (key === code[index]) {
+      index++
+
+      if (index === code.length) {
+        alert("YOU DID IT!")
+        index = 0
+      }
+    } else {
+      index = 0
     }
-  } else {
-    index = 0;
   }
 }
-}
+
+init()
